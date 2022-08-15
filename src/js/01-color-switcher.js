ce47b167ccb05e58d46afb347d17cc53f7a2f8bd
Maksi16 <maksim.moroz.kr@gmail.com>
2022-08-16 00:12:14 +0300

    task 01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+const refs = {
+  bodyEl: document.querySelector('body'),
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+};
 
+refs.startBtn.addEventListener('click', onStartBtnClick);
+refs.stopBtn.addEventListener('click', onStopBtnClick);
+
+let timerId = null;
+
+function onStartBtnClick() {
+  timerId = setInterval(() => {
+    refs.bodyEl.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  refs.startBtn.disabled = true;
+  refs.stopBtn.disabled = false;
+}
+
+function onStopBtnClick() {
+  clearInterval(timerId);
+  refs.startBtn.disabled = false;
+  refs.stopBtn.disabled = true;
+}
